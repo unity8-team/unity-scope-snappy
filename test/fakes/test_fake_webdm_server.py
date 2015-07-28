63edd59e616eed7260bdebaf33e2eead4aadd8ed
Kyle Fazzari <kyle@canonical.com>
2015-07-28 13:49:47 -0400

    Add license information in file headers.

--- test/fakes/test_fake_webdm_server.py
+++ test/fakes/test_fake_webdm_server.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2015 Canonical Ltd.
+#
+# This file is part of unity-scope-snappy.
+#
+# unity-scope-snappy is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# unity-scope-snappy is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# unity-scope-snappy. If not, see <http://www.gnu.org/licenses/>.
+
 import testtools
 import fixtures
 import json
